8fe9a8f9e128e5663efcae9aa90ce3774f606c52
Ash-Zheng <812315431@qq.com>
2021-02-25 00:21:54 +0800

    debug

--- r1cs-core/src/impl_lc.rs
+++ r1cs-core/src/impl_lc.rs
@@ -133,7 +133,7 @@ impl<F: Field> Mul<F> for LinearCombination<F> {
 
     #[inline]
     fn mul(mut self, scalar: F) -> Self {
-        println!("zheng 3");    
+        println!("Zheng 1");
         self *= scalar;
         self
     }
@@ -141,9 +141,10 @@ impl<F: Field> Mul<F> for LinearCombination<F> {
 
 impl<'a, F: Field> Mul<F> for &'a LinearCombination<F> {
     type Output = LinearCombination<F>;
+
     #[inline]
     fn mul(self, scalar: F) -> LinearCombination<F> {
-        println!("zheng 1");
+        println!("Zheng 2");
         let mut cur = self.clone();
         cur *= scalar;
         cur
@@ -151,10 +152,9 @@ impl<'a, F: Field> Mul<F> for &'a LinearCombination<F> {
 }
 
 impl<F: Field> MulAssign<F> for LinearCombination<F> {
-
     #[inline]
     fn mul_assign(&mut self, scalar: F) {
-        println!("zheng 2");
+        println!("Zheng 3");
         self.0.iter_mut().for_each(|(coeff, _)| *coeff *= &scalar);
     }
 }
@@ -472,7 +472,7 @@ impl<F: Field> Sub<(F, &LinearCombination<F>)> for &LinearCombination<F> {
 }
 
 impl<'a, F: Field> Sub<(F, &'a LinearCombination<F>)> for LinearCombination<F> {
-    type Output = <F>;
+    type Output = LinearCombination<F>;
 
     fn sub(self, (coeff, other): (F, &'a LinearCombination<F>)) -> LinearCombination<F> {
         self + (-coeff, other)
